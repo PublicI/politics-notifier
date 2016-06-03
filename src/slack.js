b62472a41aee3e0fb680eda78cee15d043316cfb
Chris Zubak-Skees <chriszs@gmail.com>
2016-06-03 19:48:09 -0400

    Factored PubSub out

--- src/slack.js
+++ src/slack.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     slack = require('@slack/client'),
     moment = require('moment'),
-    pubsub = require('./pubsub');
+    PGPubsub = require('pg-pubsub');
 
 var committees = fs.readFileSync(__dirname + '/../committees.txt','utf8').split('\n');
 
@@ -13,13 +13,13 @@ var RTM_CLIENT_EVENTS = slack.CLIENT_EVENTS.RTM;
 
 var filing_lookup = {};
 
-pubsub.subscribe('fecImportStart');
+var pubsub = new PGPubsub(process.env.DB_DRIVER + '://' +
+    process.env.DB_USER + ':' + process.env.DB_PASS + '@' +
+    process.env.DB_HOST + ':' + process.env.DB_PORT + '/' +
+    process.env.DB_NAME);
 
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
-    pubsub.on('fecImportStart',function (filing) {
-
-        console.log(filing);
-
+    pubsub.addChannel('fecimportstart',function (filing) {
         if (!(filing.filing_id in filing_lookup) &&
             committees.indexOf(filing.filer_committee_id_number) !== -1) {
 
@@ -32,10 +32,14 @@ rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
             var message = filing.committee_name +
                             ' (' + filing.filer_committee_id_number + ') filed a ' +
                             filing.form_type + ' for the period ending ' +
-                            moment(new Date(filing.coverage_through_date)).add(1,'day').format('M/D/YYYY') +
+                            moment(new Date(filing.coverage_through_date)).add(1,'day').format('M/D/YYYY') + ' ' +
                             url;
 
-            rtm.sendMessage(message, process.env.SLACK_CHANNEL, function () {
+            rtm.sendMessage(message, process.env.SLACK_CHANNEL, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+
                 console.log(message);
             });
         }
